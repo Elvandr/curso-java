84bee4fc85ce2c7f1b1a92921af9429196959f32
Elvandr <juninhosilveira231@gmail.com>
2023-01-31 18:38:15 -0300

    Initial commit

---
+++ src/numeiros.java
@@ -0,0 +1,18 @@
+public class numeiros {
+
+
+    public static void main(String[] args){
+
+          int nota1 =80;
+          int nota2 =70;
+          int nota3 =50;
+          int nota4 =88;
+
+         System.out.println("Media das notas ="+ (nota1 +nota2 +nota3 +nota4) / 88);
+
+
+
+    }
+
+
+}
